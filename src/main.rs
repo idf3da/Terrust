ee598a202033461e4a85f8d5fa44ec620ed40be8
idf3da <idf3da97df@hotmail.com>
2022-09-26 22:25:38 +0200

    Normals

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,46 @@ mod terrain;
 
 use bevy::{
 	pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
+	pbr::{MaterialPipeline, MaterialPipelineKey},
 	prelude::*,
-	render::{render_resource::WgpuFeatures, settings::WgpuSettings},
+	render::{
+		mesh::MeshVertexBufferLayout,
+		render_resource::{
+			WgpuFeatures,
+			RenderPipelineDescriptor,
+			ShaderRef,
+		}, 
+		settings::WgpuSettings,
+	},
 };
-use bevy_flycam::PlayerPlugin;
+use bevy_flycam::{PlayerPlugin, MovementSettings};
+
 
 const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+const SUN: Color = Color::rgb(0.992156, 0.721568, 0.074509);
+const S: u32 = 0;
+const SIZE: (u32, u32) = (2 << S, 2 << S);
+const HALF_SIZE: f32 = 1.0;
+
+struct CubemapMaterial {
+	base_color_texture: Option<Handle<Image>>,
+}
+
+impl Material for CubemapMaterial {
+	fn fragment_shader() -> ShaderRef {
+		"shaders/cubemap_unlit.wgsl".into()
+	}
+
+	fn specialize(
+		_pipeline: &MaterialPipeline<Self>,
+		descriptor: &mut RenderPipelineDescriptor,
+		_layout: &MeshVertexBufferLayout,
+_key: MaterialPipelineKey<Self>,
+	) -> Result<(), SpecializedMeshPipelineError> {
+		descriptor.primitive.cull_mode = None;
+		Ok(())
+	}
+}
 
 fn main() {
 	App::new()
@@ -19,36 +53,70 @@ fn main() {
 			..default()
 		})
 		.insert_resource(WgpuSettings {
-			features: WgpuFeatures::POLYGON_MODE_POINT,
+			features: WgpuFeatures::POLYGON_MODE_LINE,
 			..default()
 		})
 		.add_plugins(DefaultPlugins)
 		.add_plugin(WireframePlugin)
 		.add_plugin(PlayerPlugin)
+		.insert_resource(MovementSettings {
+			sensitivity: 0.0002,
+			speed: 4096.0,
+		})
 		.add_startup_system(setup)
 		.run();
 }
 
-/// set up a simple 3D scene
 fn setup(
 	mut commands: Commands,
 	mut wireframe_config: ResMut<WireframeConfig>,
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-	wireframe_config.global = false;
+	wireframe_config.global = true;
+
+	commands.spawn_bundle(DirectionalLightBundle {
+		directional_light: DirectionalLight {
+			illuminance: 30000.0,
+			..default()
+		},
+		transform: Transform::from_xyz(40000.0, 2000.0, 40000.0)
+			.with_rotation(Quat::from_rotation_x(-3.14 / 4.)),
+		..default()
+	});
 
 
-	commands.spawn_bundle(PointLightBundle {
-		transform: Transform::from_translation(Vec3::new(32.0, 16.0, 32.0)),
+	commands.spawn_bundle(PbrBundle {
+		transform: Transform::from_scale(Vec3::splat(75.0)),
+
+		mesh: meshes.add(terrain::generate_grid(SIZE)),
+		material: materials.add(StandardMaterial {
+			base_color: Color::rgb(1.0, 1.0, 1.0),
+			..default()
+		}),
 		..Default::default()
 	});
 
-	commands
-		.spawn_bundle(PbrBundle {
-			mesh: meshes.add(terrain::generate_grid((64, 64))),
-			material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-			..Default::default()
-		})
-		.insert(Wireframe);
+	commands.spawn_bundle(PbrBundle {
+		transform: Transform::from_translation(Vec3::new(40000.0, 2000.0,40000.0)),
+		mesh: meshes.add(Mesh::from(shape::Icosphere {
+			radius: 200.0,
+			subdivisions: 3,
+		})),
+		material: materials.add(StandardMaterial {
+			base_color: Color::rgb(1.0, 1.0, 1.0),
+			..default()
+		}),
+		
+	        ..Default::default()
+	});
+
+	commands.spawn(MaterialMeshBundle::<CubemapMaterial> {
+        	mesh: meshes.add(Mesh::from(shape::Cube { size: 10000.0 })),
+                material: cubemap_materials.add(CubemapMaterial {
+                	base_color_texture: Some(cubemap.image_handle.clone_weak()),
+                }),
+                ..default()
+            });
+	
 }
--- src/terrain.rs
+++ src/terrain.rs
@@ -1,27 +1,61 @@
 use bevy::prelude::*;
-use rand::Rng;
+// use rand::Rng;
+use noise::{NoiseFn, Perlin};
+
+fn type_of<T>(_: T){
+	println!("{}", std::any::type_name::<T>());
+}
+
 
 pub fn generate_grid(size: (u32, u32)) -> Mesh {
+
+        println!("Grid of {} by {}", size.0, size.1);
+
         let mut mesh = Mesh::new(bevy::render::mesh::PrimitiveTopology::TriangleList);
+        let pos = generate_positions(size);
 
         mesh.set_indices(Some(bevy::render::mesh::Indices::U32(generate_indicies(size))));
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, generate_positions(size));
-mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL,vec![[0.0, 1.0, 0.0]; ((size.0 + 1) * (size.1 + 1)) as usize]);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, pos.clone());
+        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, generate_normals(size, pos.clone()));
         // mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[1.0, 1.0]; ((size.0 + 1) * (size.1 + 1)) as usize]);
 
         return mesh;
 }
 
 fn generate_positions(size: (u32, u32)) -> Vec<[f32; 3]> {
-	let mut vert = Vec::new();
+	let mut vert: Vec<[f32; 3]> = Vec::new();
         let mut rng = rand::thread_rng();
+        let perlin = Perlin::new(); // TODO: Rewrite cuz min and max are capped?
+        
+        let scale2d1st = 0.00050;
+        let scaley1st = 350.0;
+
+        let scale2d2nd = scale2d1st * 4.0; 
+        let scaley2nd = scaley1st / 2.0;
+        let weight2nd = 1.25;
+
+        let scale2d3rd = scale2d2nd * 4.0;
+        let scaley3rd = scaley2nd / 2.0;
+        let weight3rd = 0.5;
+
+        let scale2d4th = scale2d3rd * 4.0;
+        let scaley4th = scaley3rd / 2.0;
+        let weight4th = 0.0625;
 
 	for x in 0..size.0+1 {
 		for z in 0..size.1+1 {
-			// vert.push([x as f32, 0.0, z as f32]);
-                        vert.push([x as f32, rng.gen_range(-1.0..3.0), z as f32]);
+
+                        let y1 = perlin.get([(x as f64 * scale2d1st + 0.5), (z as f64 * scale2d1st + 0.5)]) * scaley1st;
+                        let y2 = perlin.get([(x as f64 * scale2d2nd + 0.5), (z as f64 * scale2d2nd + 0.5)]) * scaley2nd * 2.0;
+                        let y3 = perlin.get([(x as f64 * scale2d3rd + 0.5), (z as f64 * scale2d3rd + 0.5)]) * scaley3rd * 8.0;
+                        let y4 = perlin.get([(x as f64 * scale2d4th + 0.5), (z as f64 * scale2d4th + 0.5)]) * scaley4th * 16.0;  
+
+                        let finaly = (y1 + y2 * weight2nd + y3 * weight3rd + y4 * weight4th) / 4.0;
+			vert.push([x as f32, finaly as f32, z as f32]);
 		}
 	}
+
+        println!("Pos count: {}", vert.len());
 
 	return vert;
 }
@@ -31,7 +65,6 @@ fn generate_indicies(size: (u32, u32)) -> Vec<u32> {
 
 	let mut vert: u32 = 0;
 	let mut tris: usize = 0;
-
 
 	for _ in 0..size.0 {
 		for _ in 0..size.1 {
@@ -50,5 +83,51 @@ fn generate_indicies(size: (u32, u32)) -> Vec<u32> {
                 vert += 1
         }
 
+        println!("Idx count: {}", idx.len());
+
         return idx;
+}
+
+// TODO: Some Vec to Vec
+
+fn generate_normals(size: (u32, u32), positions: Vec<[f32; 3]>) -> Vec<[f32; 3]> {
+        let mut normals: Vec<[f32; 3]> = Vec::new();
+        let mut A: [f32; 3];
+        let mut B: [f32; 3];
+        let mut C: [f32; 3];
+        let mut D: [f32; 3];
+        let mut c = 0;
+        for i in 0..size.0 {
+                for j in 0..size.1 {
+                        A = vec_sub(positions[(i + j * size.0) as usize], positions[(i + j * size.0 + 1) as usize]);
+                        B = vec_sub(positions[(i + j * size.0) as usize], positions[(i + (j + 1) * size.0) as usize]);
+
+                        if (i + 1) + (j + 1) * size.0 != positions.len() as u32 {       
+                                C = vec_sub(positions[(i + j * size.0 + 1) as usize], positions[((i + 1) + (j + 1) * size.0) as usize]);
+                                D = vec_sub(positions[(i + (j + 1) * size.0) as usize], positions[((i + 1) + (j + 1) * size.0) as usize]);
+                        } else {
+                                C = [0.0, 0.0, 0.0];
+                                D = [0.0, 0.0, 0.0];
+                        }
+                        
+                        normals.push(cross_prod(A, B));
+                        normals.push(cross_prod(C, D));
+
+                        c += 2;
+                }
+        }
+
+        normals.push([0.0, 0.0, 0.0]);
+
+        println!("Normal count: {}", c);
+
+        return normals;
+}
+
+fn vec_sub(A: [f32; 3], B: [f32; 3]) -> [f32; 3] {
+        return [B[0] - A[0], B[1] - A[1], B[2] - A[2]];        
+}
+
+fn cross_prod (A: [f32; 3], B: [f32; 3]) -> [f32; 3] {
+        return [A[1]*B[2] - A[2]*B[1], A[2]*B[0]-A[0]*B[2], A[0]*B[1]-A[1]*B[0]];
 }
